3ff99f46f3430aa539f6575fe7c127ba6d51dc66
Maheswar Lal <maheswar.lal@cellpointmobile.com>
2024-05-05 13:36:23 +0530

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/maheswarlal/puppy
 
 go 1.22.2
+
+require github.com/maheswarlal/dog v0.0.0-20240505074041-a7fdcc4adf78 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,16 @@
 package puppy
 
+import (
+	"github.com/maheswarlal/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
 }
